35460521132fe7fabac1f64dd085ab351eb68b0a
burglar123 <rzwang@hust.edu.cn>
2026-05-23 17:32:03 +0800

    Tighten cached-admission mode checks and token/arrival accounting

--- nano_pearl/pearl_engine/sequence.py
+++ nano_pearl/pearl_engine/sequence.py
@@ -8,6 +8,7 @@ from ..layers.sampler import SamplingParams
 
 class SequenceStatus(Enum):
     WAITING = auto()
+    PENDING_CACHED = auto()
     RUNNING = auto()
     FINISHED = auto()
 
@@ -29,7 +30,9 @@ class Sequence:
         self.seq_id = next(Sequence.counter)
         self.request_id = self.seq_id if request_id is None else request_id
         self.arrival_ts = time.time() if arrival_ts is None else arrival_ts
+        self.arrival_offset_sec = None
         self.first_token_ts = None
+        self.admit_ts = None
         self.finish_ts = None
         self.decode_ready_ts = None
         self.decode_start_ts = None
@@ -162,8 +165,10 @@ class Sequence:
             "seq_id": self.seq_id,
             "request_id": self.request_id,
             "arrival_ts": self.arrival_ts,
+            "arrival_offset_sec": self.arrival_offset_sec,
             "first_token_ts": self.first_token_ts,
             "finish_ts": self.finish_ts,
+            "admit_ts": self.admit_ts,
             "decode_ready_ts": self.decode_ready_ts,
             "decode_start_ts": self.decode_start_ts,
             "decode_ready_mode": self.decode_ready_mode,
@@ -181,7 +186,8 @@ class Sequence:
         return (self.num_tokens, self.num_prompt_tokens, self.num_cached_tokens, self.block_table,
                 self.temperature, self.ignore_eos, self.max_tokens, self.seq_id, self.pre_verify,
                 self.num_acc_tokens, self.cur_acc_tokens, self.request_id, self.arrival_ts,
-                self.first_token_ts, self.finish_ts, self.decode_ready_ts, self.decode_start_ts,
+                self.arrival_offset_sec,
+                self.first_token_ts, self.admit_ts, self.finish_ts, self.decode_ready_ts, self.decode_start_ts,
                 self.decode_ready_mode, self.num_decode_ready_prefill_tokens,
                 self.slo_tpot_ms, self.slo_class, self.per_request_gamma, self.trace_stats,
                 self.token_ids if self.num_completion_tokens == 0 else self.last_token)
@@ -190,7 +196,8 @@ class Sequence:
         (self.num_tokens, self.num_prompt_tokens, self.num_cached_tokens, self.block_table,
          self.temperature, self.ignore_eos, self.max_tokens, self.seq_id, self.pre_verify,
          self.num_acc_tokens, self.cur_acc_tokens, self.request_id, self.arrival_ts,
-         self.first_token_ts, self.finish_ts, self.decode_ready_ts, self.decode_start_ts,
+         self.arrival_offset_sec,
+         self.first_token_ts, self.admit_ts, self.finish_ts, self.decode_ready_ts, self.decode_start_ts,
          self.decode_ready_mode, self.num_decode_ready_prefill_tokens,
          self.slo_tpot_ms, self.slo_class, self.per_request_gamma, self.trace_stats) = state[:-1]
         if self.num_completion_tokens == 0:
